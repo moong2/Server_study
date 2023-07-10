b08df959e0cbd2e4fec30f769a31d6fd6ac4d72f
moong2 <www.castlehi@gmail.com>
2023-07-10 17:17:08 +0900

    feat: base IRC programming in C and test with telnet [success]

--- IRC/server.c
+++ IRC/server.c
@@ -5,25 +5,71 @@
 #include <stdio.h>
 #include <string.h>
 #include <fcntl.h>
+#include <sys/event.h>
 
-#define BUFSIZE 1024
+#define BUFSIZE 512
 
-void error_handling(char* message) {
+// 서버 소켓
+int g_serv_sock;
+// 이벤트 큐
+int g_event_queue;
+
+void error_handling(char* message) 
+{
 	fputs(message, stderr);
 	fputc('\n', stderr);
 	exit(1);
 }
 
-int main(int argc, char **argv) {
-	int serv_sock;
+void add_socket(int socket) 
+{
+	struct kevent event;
+	EV_SET(&event, socket, EVFILT_READ | EVFILT_WRITE, EV_ADD, 0, 0, 0);
+	kevent(g_event_queue, &event, 1, 0, 0, 0);
+}
+
+void success_handler(int socket)
+{
+	//서버 소켓일 경우 클라이언트 소켓의 접속 요청
+	if (g_serv_sock == socket)
+	{
+		int client_sock = accept(g_serv_sock, 0, 0);
+		if (client_sock == -1)
+			error_handling("socket accept error");
+
+		if (fcntl(client_sock, F_SETFL, O_NONBLOCK) == -1)
+			error_handling("client fcntl error");
+	
+		add_socket(client_sock);
+	}
+	else
+	{
+		//클라이언트 소켓일 경우 채팅 요청
+		char message[BUFSIZE];
+
+		int nRcv = recv(socket, message, sizeof(message) - 1, 0);
+		if (nRcv < 1)
+		{
+			//클라이언트 종료
+		}
+
+		message[nRcv] = '\0';
+
+		printf("ReceiveMessage : %s\n", message);
+
+		char *check_message = "Server accept\n\0";
+		send(socket, check_message, (int)strlen(check_message), 0);
+	}
+}
+
+int main(int argc, char **argv) 
+{
 	int clnt_sock;
 
 	//sockaddr_in은 소켓 주소의 틀을 형성해주는 구조체로 AF_INET(IPv4)일 경우 사용
 	struct sockaddr_in serv_addr;
 	struct sockaddr_in clnt_addr; //accept 함수에서 사용됨
 	socklen_t clnt_addr_size;
-
-	char message[BUFSIZE];
 
 	if (argc != 2)
 	{
@@ -32,16 +78,16 @@ int main(int argc, char **argv) {
 	}
 
 	//TCP 연결지향형이고 IPv4 도메인을 위한 소켓 생성
-	if ((serv_sock = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+	if ((g_serv_sock = socket(AF_INET, SOCK_STREAM, 0)) == -1)
 		error_handling("socket error");
 
 	//SO_REUSEADDR 옵션 설정
 	int option = 1;
-	if (setsockopt(serv_sock, SOL_SOCKET, SO_REUSEADDR, &option, sizeof(option)) == -1)
+	if (setsockopt(g_serv_sock, SOL_SOCKET, SO_REUSEADDR, &option, sizeof(option)) == -1)
 		error_handling("setsock error");
 
 	//NONBLOCK 플래그 설정
-	if (fcntl(serv_sock, F_SETFL, O_NONBLOCK) < 0)
+	if (fcntl(g_serv_sock, F_SETFL, O_NONBLOCK) < 0)
 		error_handling("fcntl error");
 
 	char *end;
@@ -54,14 +100,55 @@ int main(int argc, char **argv) {
 	serv_addr.sin_port = htons(port);	//포트
 
 	//소켓과 서버 주소를 바인딩
-	if (bind(serv_sock, (struct sockaddr*) &serv_addr, sizeof(serv_addr)) == -1)
+	if (bind(g_serv_sock, (struct sockaddr*) &serv_addr, sizeof(serv_addr)) == -1)
 		error_handling("bind error");
 
 	//연결 대기열 2개 생성
-	if (listen(serv_sock, 2) == -1)
+	if (listen(g_serv_sock, 2) == -1)
 		error_handling("listen error");
-	
-	/* reactor - kqueue, kevent 활용 후 이벤트 감지 */
+
+	//kqueue 생성
+	if ((g_event_queue = kqueue()) < 0)
+		error_handling("kqueue error");
+
+	//서버 소켓 kqueue, kevent 연결
+	add_socket(g_serv_sock);
+
+	//이벤트 루프
+	while(1)
+	{
+		//이벤트 감지
+		const int MAX_EVENT = 100;
+
+		struct kevent events[MAX_EVENT];
+		int nEvents = kevent(g_event_queue, 0, 0, events, MAX_EVENT, 0);
+
+		for (int i = 0; i < nEvents; i++)
+		{
+			struct kevent* cur_event = &events[i];
+
+			if (cur_event->filter == EVFILT_WRITE)
+			{
+				printf("This is write");
+				exit(1);
+			}
+			if (cur_event->filter == EVFILT_READ)
+			{
+				success_handler(cur_event->ident);
+			}
+			if (cur_event->flags & EV_EOF)
+			{
+				printf("This is EOF");
+				exit(1);
+			}
+			if (cur_event->flags & EV_ERROR)
+			{
+				printf("This is error");
+				exit(1);
+			}
+		}
+	}
+
 
 	return 0;
 }
